1946e50bbb77894b135819d5fae714f2ad88e416
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-10-20 17:31:36 -0600

    Método para la inscripción de asignaturas

--- EstudioDeCaso/Program.cs
+++ EstudioDeCaso/Program.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EstudioCaso2
 {
     internal class Program
     {
-        static void Main()
+        static Queue<string> colaSolicitudes = new Queue<string>();
+        static void InscribirAsignaturas()
         {
+            Console.WriteLine("Ingrese el nombre de la asignatura a inscribir (o 'salir' para terminar):");
+
+            while (true)
+            {
+                string asignatura = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(asignatura))
+                {
+                    Console.WriteLine("Ingrese un nombre válido o 'salir' para terminar:");
+                    continue;
+                }
+
+                if (asignatura.ToLower() == "salir")
+                    break;
+
+                colaSolicitudes.Enqueue(asignatura);
+                Console.WriteLine($" '{asignatura}' agregada correctamente. Ingrese otra asignatura o 'salir' para terminar:");
+            }
+
+            Console.WriteLine("\n--- Asignaturas inscritas ---");
+            foreach (var materia in colaSolicitudes)
+            {
+                Console.WriteLine($"- {materia}");
+            }
         }
     }
 }
